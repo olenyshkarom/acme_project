98ec7f301ebf73d86b4bbddebe9e0150e6eff8e4
Ольга Каракян <olenyshkarom@yandex.ru>
2025-09-06 22:25:10 +0300

    Формы из моделей

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,17 +1,50 @@
 from django import forms
 
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(
-        label='Фамилия', required=False, help_text='Необязательное поле') # required=True указывать не требуется
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+
+class BirthdayForm(forms.ModelForm):
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        model = Birthday    # Указываем модель, на основе которой должна строиться форма.
+        fields = '__all__'  # Указываем, что надо отобразить все поля.
+
+        # для поля с датой рождения используется виджет с типом данных date.
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})    # Виджеты полей описываются в подклассе Meta:
+        }
 
 # IntegerField для целочисленных полей,
 # CharField для текстовых полей,
 # DateField для полей с датой.
 
 # В аргументе attrs задан атрибут type="date" для тега <input>. Если HTML-форму верстают вручную, этот атрибут описывают прямо в теге: <input type="date">.
+# python manage.py makemigrations
+# python manage.py migrate
+
+# class BirthdayForm(forms.Form):
+#     first_name = forms.CharField(label='Имя', max_length=20)
+#     last_name = forms.CharField(
+#         label='Фамилия', required=False, help_text='Необязательное поле') # required=True указывать не требуется
+#     birthday = forms.DateField(
+#         label='Дата рождения',
+#         widget=forms.DateInput(attrs={'type': 'date'})
+#     )
+
+class ContestForm(forms.Form):
+    title = forms.CharField(label='Название', max_length=20)
+    description = forms.CharField(
+        label='Описание',
+        widget=forms.Textarea({'cols': '22', 'rows': '5'}),
+    )
+    price = forms.IntegerField(
+        label='Цена',
+        min_value=10, max_value=100,
+        help_text='Рекомендованная розничная цена',
+    )
+    comment = forms.CharField(
+        label='Комментарий',
+        required=False,
+        widget=forms.Textarea({'cols': '22', 'rows': '5'}),
+    )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,49 @@
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения')
+
+# blank=True - допустимы пустые значения
+# для полей модели с классом models.CharField обязательно должна быть указана максимальная длина поля; в нашем случае — max_length=20
+# кастомное название поля формы указывается в параметре label, а в модели это название указывают первым позиционным аргументом или в параметре verbose_name.
+
+
+
+
+
+# from django.db import models
+
+# from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+# # Опишите модель Contest здесь!
+# class Contest(models.Model):
+#     title = models.CharField('Название', max_length=20)
+#     description = models.TextField('Описание')
+#     price = models.IntegerField(
+#         'Цена',
+#         validators=[MinValueValidator(10), MaxValueValidator(100)],
+#         help_text='Рекомендованная розничная цена'
+#     )
+#     comment = models.TextField('Комментарий', blank=True)
+
+# from django import forms
+
+# from .models import Contest
+
+
+# class ContestForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Contest
+#         fields = '__all__'
+#         widgets= {
+#             'comment': forms.Textarea({'cols': '22', 'rows': '5'}),
+#             'description': forms.Textarea({'cols': '22', 'rows': '5'})
+#         }
+
