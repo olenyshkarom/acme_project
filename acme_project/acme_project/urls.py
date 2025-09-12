ab7c4af0a27bba12bb04eb878335f4d2d909e047
Ольга Каракян <olenyshkarom@yandex.ru>
2025-09-12 22:06:19 +0300

    Работа с изображениями в формах

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,3 +1,7 @@
+# Импортируем настройки проекта.
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,4 +9,8 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-]
+    # В конце добавляем к списку вызов функции static.
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # исключительно для этапа разработки
+
+# Этот способ будет работать только в режиме разработки (когда проект запускается командой python manage.py runserver) и при настройке DEBUG = True в settings.py. 
+# потом заменить на DEBUG = False
