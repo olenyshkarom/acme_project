89955d41fcc8a18eea4bed183db55d58b1c13a6e
Ольга Каракян <olenyshkarom@yandex.ru>
2025-10-06 23:37:02 +0300

    НАстройка страницы регистрации

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,7 +3,9 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -11,6 +13,18 @@ urlpatterns = [
     path('birthday/', include('birthday.urls')),
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
+    # Применим этот подход: в проекте нет отдельного
+    # приложения для работы с пользователями, и разместить CBV некуда; создавать приложение ради единственного view-класса не хочется.
+
     # В конце добавляем к списку вызов функции static.
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # исключительно для этапа разработки
 
